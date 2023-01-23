6bab5bddb45e25588cf9235d404547d7c090935d
evance-14 <kiropevans097@gmail.com>
2023-01-23 17:47:33 +0300

    added a delete request

--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -20,9 +20,36 @@ const BotCollection = () => {
             .then(res => res.json())
             .then(data => setSelectedBots(data));
     });
+
+     // Removes a bot from the soldiers array by filtering out the bot that matches the passed in bot's id
+     const removeSoldier = bot => {
+        setSoldiers(soldiers.filter(s => s.id !== bot.id));
+    };
+
+      
+    // Save the soldiers array to local storage whenever it is updated
+    useEffect(() => {
+        localStorage.setItem('soldiers', JSON.stringify(soldiers));
+    }, [soldiers]);
+  
+    const addSoldier = bot => {
+        if (!soldiers.find(s => s.id === bot.id)) {
+            setSoldiers([...soldiers, bot]);
+        }
+    };
+    // Delete a bot from the soldiers array by filtering out the bot that matches the passed in bot's id
+    // and make a DELETE request to the api for that bot
+    const deleteSoldier = botId => {
+        setSoldiers(soldiers.filter(s => s.id !== botId));
+        fetch(`http://localhost:8001/bots/${botId}`, {
+            method: 'DELETE',
+        },[]);
+
+
+    };
      return(
         <div>
-            
+
         </div>
      )
 };
